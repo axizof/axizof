cefef833a248f645803599afdd5f5f6c2e4632d0
Axiz <38507086+axizof@users.noreply.github.com>
2023-10-03 16:42:12 +0200

    Update test.bash

--- test.bash
+++ test.bash
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Répertoire où vous souhaitez créer les répertoires
-base_directory="/var/www/html/hack/"
+base_directory="/chemin/vers/votre/repertoire"
+
+# Fonction pour générer un nom aléatoire
+generate_random_name() {
+  echo "$(date +%s%N | sha256sum | base64 | head -c 12)"
+}
 
 # Boucle pour créer 500 répertoires
 for ((i = 1; i <= 500; i++)); do
-  # Génère un nom aléatoire en utilisant uuidgen
-  random_name=$(uuidgen)
+  # Génère un nom aléatoire
+  random_name=$(generate_random_name)
 
   # Crée le répertoire avec le nom aléatoire
   mkdir -p "$base_directory/$random_name"
